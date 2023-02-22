4dff26b3c961a1cb1ae0e39b6a5cf21be0a18004
Lion Cassens <l.cassens@tudelft.nl>
2023-02-22 23:15:58 +0100

    add parameter for number of observation to data generator

--- generate_synthetic_data.py
+++ generate_synthetic_data.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import numpy as np
 
-def generate_synthetic_data(BETA_TIME, BETA_COST):
+def generate_synthetic_data(BETA_TIME, BETA_COST, n_observations=None):
   df = pd.read_csv('https://raw.githubusercontent.com/TB341TC/course_material/main/ica_2_incomplete_synthetic_data.csv')
+  if n_observations:
+    if (n_observations % 2) == 1:
+        raise Exception('Only odd numbers of observations are supported')
+    df = df.iloc[:2]
+    df = pd.DataFrame(np.repeat(df.values, int(n_observations/2), axis=0), columns = df.columns)
+    df['ID'] = list(range(1, int(n_observations/2)+1)) + list(range(1, int(n_observations/2)+1))
+    df.sort_values('ID', inplace=True)
+    df.reset_index(inplace=True)
+    df.drop('index', axis=1, inplace=True)
   df['V_1'] = BETA_TIME * df['TIME_ROUTE_1'] + BETA_COST * df['COST_ROUTE_1']
   df['V_2'] = BETA_TIME * df['TIME_ROUTE_2'] + BETA_COST * df['COST_ROUTE_2']
   df['e_1'] = np.random.gumbel(size=df.shape[0])
